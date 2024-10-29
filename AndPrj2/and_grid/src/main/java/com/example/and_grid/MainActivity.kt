f7016b04b89a4dd7d99776a5d5535958eaa735ac
Anchaeyeon <s2328@e-mirim.hs.kr>
2024-10-29 19:52:51 +0900

    버튼 누르면 토스트 나오게 하기 & 취소하면 사라지게 하기

--- AndPrj2/and_grid/src/main/java/com/example/and_grid/MainActivity.kt
+++ AndPrj2/and_grid/src/main/java/com/example/and_grid/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.and_grid
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.and_grid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnLogin.setOnClickListener {
+            var id = "아이디 : " + binding.edtID.text.toString()
+            var pw = "\n비밀번호 : " + binding.edtPW.text.toString()
+            Toast.makeText(applicationContext, id + pw, Toast.LENGTH_SHORT).show()
+        }
+        binding.btnCancel.setOnClickListener {
+            binding.edtID.setText("")
+            binding.edtPW.setText("")
         }
     }
 }
