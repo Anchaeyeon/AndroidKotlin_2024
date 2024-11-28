5e1228219ba33d71d3b62bb88e10ec6266cb07cb
Anchaeyeon <s2328@e-mirim.hs.kr>
2024-11-28 19:13:19 +0900

    여러 개 선택 가능하게

--- AndProject3/andlistview1/src/main/java/com/example/andlistview1/MainActivity.kt
+++ AndProject3/andlistview1/src/main/java/com/example/andlistview1/MainActivity.kt
@@ -18,12 +18,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         var mid = arrayOf("히어로즈", "24시", "로스트", "로스트룸", "스몰빌", "탐정몽크",
             "빅뱅이론", "프렌즈", "덱스터", "글리", "가쉽걸", "테이큰", "슈퍼내추럴", "브이")
-        var adapter : ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, mid)
-        //생성한 ArrayAdapter를 리스트뷰에 적용
-        //binding.listView1.adapter = adapter
-
+        var adapter : ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, mid)
         //한 개만 고르기 가능하게
         //binding.listView1.choiceMode = ListView.CHOICE_MODE_SINGLE
+
+        //여러개 선택 가능
+        binding.listView1.choiceMode = ListView.CHOICE_MODE_MULTIPLE
+
+        //생성한 ArrayAdapter를 리스트뷰에 적용
+        binding.listView1.adapter = adapter
 
         binding.listView1.setOnItemClickListener { parent, view, position, id ->
             Toast.makeText(applicationContext, mid[position], Toast.LENGTH_SHORT).show()
