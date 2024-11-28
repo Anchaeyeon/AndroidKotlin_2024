32fbacb0344ebb09b623fb0876ab03b4bff57e10
Anchaeyeon <s2328@e-mirim.hs.kr>
2024-11-28 19:05:41 +0900

    리스트로 텍스트만 쭉 나오게 하기

---
+++ AndProject3/andlistview1/src/main/java/com/example/andlistview1/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.andlistview1
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.andlistview1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var mid = arrayOf("히어로즈", "24시", "로스트", "로스트룸", "스몰빌", "탐정몽크",
+            "빅뱅이론", "프렌즈", "덱스터", "글리", "가쉽걸", "테이큰", "슈퍼내추럴", "브이")
+        var adapter : ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, mid)
+        binding.listView1.adapter = adapter
+
+        binding.listView1.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(applicationContext, mid[position], Toast.LENGTH_SHORT).show()
+        }
+    }
+}
