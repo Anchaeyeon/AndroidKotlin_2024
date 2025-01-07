eed2fe591decb4317cc374794515901cc77c527a
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-01-07 15:53:21 +0900

    [겨울방학 방과후] 프래그먼트로 화면 이동 가능하도록 코드 작성

---
+++ AndProject5/app4/src/main/java/com/example/app4/FirstFragment.kt
@@ -0,0 +1,24 @@
+package com.example.app4
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class FirstFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //뷰 초기화 및 이벤트 처리
+        view.findViewById<TextView>(R.id.textView).text = "첫 번째 프래그먼트"
+    }
+}
--- AndProject5/app4/src/main/java/com/example/app4/MainActivity.kt
+++ AndProject5/app4/src/main/java/com/example/app4/MainActivity.kt
@@ -5,16 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.app4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //프래그먼트 추가하기
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, FirstFragment()).commit()
         }
+        binding.btnSwitch.setOnClickListener { switchFragment() }
+    }
+
+    private fun switchFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        //현재 프래그먼트 확인
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (currentFragment is FirstFragment) {
+            transaction.replace(R.id.fragment_container, SecondFragment())
+        }
+        else {
+            transaction.replace(R.id.fragment_container, FirstFragment())
+        }
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
