4ff96d061ec2d73fb4b5e01aa89f5a11cd56dd60
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-01-07 09:16:34 +0900

    [겨울방학 방과후] 옵션 선택해서 그에 맞는 연산하기

--- AndProject4/andtest06/src/main/java/com/example/andtest06/MainActivity.kt
+++ AndProject4/andtest06/src/main/java/com/example/andtest06/MainActivity.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.andtest06.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.op.setSelection(0) //기본연산자 +
+        binding.btnResult.setOnClickListener {
+            var x = binding.edX.text.toString().toInt()
+            var y = binding.edY.text.toString().toInt()
+            var result = 0
+
+            when (binding.op.selectedItemPosition) {
+                0 -> result = x+y
+                1 -> result = x-y
+                2 -> result = x*y
+                3 -> result = x%y
+            }
+            binding.txtResult.text = result.toString()
         }
     }
 }
