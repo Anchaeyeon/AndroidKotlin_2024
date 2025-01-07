c93de85f526ecc2632fbe06b3513184589ba82eb
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-01-07 14:42:47 +0900

    [겨울방학 방과후] 다이얼로그로 화면에 출력

--- AndProject5/app3/src/main/java/com/example/app3/MainActivity.kt
+++ AndProject5/app3/src/main/java/com/example/app3/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.app3
 
+import android.os.Binder
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,6 +17,29 @@ import com.example.app3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        var mInflate = menuInflater
+        mInflate.inflate(R.menu.menu1, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.itemExit -> finish()
+            R.id.itemClear -> {
+                binding.edName.setText("")
+                binding.edMid.setText("")
+                binding.edFinal.setText("")
+                binding.edReport.setText("")
+                binding.edAttend.setText("")
+                binding.rg.clearCheck()
+            }
+        }
+        return true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -31,18 +60,33 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnCal.setOnClickListener {
+            val dlgView = View.inflate(this@MainActivity, R.layout.dlg, null)
             val sum = (binding.edMid.getText().toString().toDouble() * 0.3 +
                         binding.edFinal.getText().toString().toDouble() * 0.3 +
                         binding.edReport.getText().toString().toDouble() * 0.2 +
                         binding.edAttend.getText().toString().toDouble() * 0.2).toInt()
 
-            var str = "F"
-            if (sum >= 90) str = "A"
-            else if (sum >= 80) str = "B"
-            else if (sum >= 70) str = "C"
-            else str = "F"
+            var score = dlgView.findViewById<TextView>(R.id.txtScore)
+            var img = dlgView.findViewById<ImageView>(R.id.img)
+            var str = ""
 
-            Toast.makeText(this, "학점 : " + str, Toast.LENGTH_SHORT).show()
+            when (binding.rg.checkedRadioButtonId) {
+                R.id.rb1 -> str += "1학년"
+                R.id.rb2 -> str += "2학년"
+                R.id.rb3 -> str += "3학년"
+            }
+
+            score.text = str + binding.edName.text.toString() + "학생의 총점: " + sum.toString()
+            if (sum >= 90) img.setImageResource(R.drawable.alphabet_a)
+            else if (sum >= 80) img.setImageResource(R.drawable.alphabet_b)
+            else if (sum >= 70) img.setImageResource(R.drawable.alphabet_c)
+            else img.setImageResource(R.drawable.alphabet_f)
+
+            val dlg = AlertDialog.Builder(this@MainActivity)
+            dlg.setView(dlgView)
+            dlg.setTitle("학점계산결과")
+            dlg.setPositiveButton("확인", null)
+            dlg.show()
         }
     }
 }
