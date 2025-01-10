26ea289648a252867bddaf3aeadfff50bc69128c
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-01-10 14:25:23 +0900

    [겨울방학 방과후] 인텐트 - 다른 화면으로 넘어가서 할 일 추가 가능하게

--- AfterSchool1/andtest1/src/main/java/com/example/andtest1/MainActivity.kt
+++ AfterSchool1/andtest1/src/main/java/com/example/andtest1/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.andtest1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.andtest1.databinding.ActivityMainBinding
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    var datas: MutableList<String>? = null
+    lateinit var adapter: MyAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val requestLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            val result = it.data!!.getStringExtra("result")
+            if (result!=null && result!!.toString()!="cancel") {
+                datas?.add(result)
+                adapter.notifyDataSetChanged()
+            }
         }
+        binding.mainFab.setOnClickListener {
+            val intent = Intent(this, AddActivity::class.java)
+            requestLauncher.launch(intent) //양방향
+        }
+        datas = savedInstanceState?.let {
+            it.getStringArrayList("datas")?.toMutableList()
+        } ?: let { mutableListOf<String>() }
+
+        adapter = MyAdapter(datas) // 어댑터에 데이터 세팅
+        binding.mainRecyclerView.layoutManager = LinearLayoutManager(this) // 리사이클러뷰 세팅 (레이아웃 설정)
+        binding.mainRecyclerView.adapter = adapter // 리사이클러뷰에 어댑터 연결
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putStringArrayList("datas", ArrayList(datas))
     }
 }
