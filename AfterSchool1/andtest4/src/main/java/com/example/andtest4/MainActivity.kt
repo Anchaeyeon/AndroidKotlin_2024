27eef685cad98bdfeaedce4f2f3970b13f795f6e
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-01-14 11:11:57 +0900

    [겨울방학 방과후] 네비게이션뷰 세팅 연결(툴바 세팅, 아이콘 연결)

--- AfterSchool1/andtest4/src/main/java/com/example/andtest4/MainActivity.kt
+++ AfterSchool1/andtest4/src/main/java/com/example/andtest4/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.andtest4
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.andtest4.databinding.ActivityMainBinding
 
@@ -18,8 +21,8 @@ class MainActivity : AppCompatActivity() {
 
         var adapter = MyAdapter(datas)
         datas.addAll(helper.allTodo())
-        binding.mainRecyclerView.layoutManager = LinearLayoutManager(this)
-        binding.mainRecyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
 
         binding.fabAdd.setOnClickListener {
             val intent = Intent(this, AddActivity::class.java)
@@ -33,6 +36,32 @@ class MainActivity : AppCompatActivity() {
                 intent.putExtra("todo", todo)
                 startActivity(intent)
             }
+        }
+
+        //툴바 세팅, ActionBarDrawerToggle로 홈버튼 아이콘 연결
+        setSupportActionBar(binding.toolbar)
+        val toggle = ActionBarDrawerToggle(
+            this, binding.drawer, binding.toolbar,
+            R.string.navi_drawer_open, R.string.navi_drawer_close )
+        binding.drawer.addDrawerListener(toggle)
+        toggle.syncState() //홈버튼 표시
+        binding.mainDrawerView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.item1 -> {
+                    Toast.makeText(this, "Share clicked", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item2 -> {
+                    Toast.makeText(this, "Question clicked", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item3 -> {
+                    Toast.makeText(this, "Search clicked", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item4 -> {
+                    binding.drawer.closeDrawer(GravityCompat.START); true //내비게이션 닫기
+                }
+            }
+            binding.drawer.closeDrawer(GravityCompat.START) //내비게이션 닫기
+            true //true를 반환하면 클릭 이벤트가 처리됨
         }
     }
 }
