efcc70803a36b855f0c405ae1fc69164c9b8beb2
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-01-14 14:11:26 +0900

    [겨울방학 방과후] 버튼 클릭했을 때 실행되도록

--- AfterSchool1/andtest5/src/main/java/com/example/andtest5/MainActivity.kt
+++ AfterSchool1/andtest5/src/main/java/com/example/andtest5/MainActivity.kt
@@ -5,16 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.andtest5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    val helper = MyDBHelper(this)
+    //val datas = mutableListOf<Memo>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = MyAdapter()
+        adapter.helper = helper
+        adapter.datas.addAll(helper.selectMemo())
+        binding.recyclerMemo.adapter = adapter
+        binding.recyclerMemo.layoutManager = LinearLayoutManager(this)
+
+        binding.buttonSave.setOnClickListener {
+            if (binding.editMemo.text.toString().isNotEmpty()) {
+                val memo = Memo(null, binding.editMemo.text.toString(), System.currentTimeMillis())
+                helper.insertMemo(memo)
+                adapter.datas.clear()
+                adapter.datas.addAll(helper.selectMemo())
+                adapter.notifyDataSetChanged()
+                binding.editMemo.setText("")
+            }
         }
     }
 }
