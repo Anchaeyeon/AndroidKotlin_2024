36ffc8e0a24aec2822b2d0e03ba7c117c38d9787
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-01-14 10:19:23 +0900

    [겨울방학 방과후] 클릭했을 때 실행되도록 하는 main 코드

--- AfterSchool1/andtest4/src/main/java/com/example/andtest4/MainActivity.kt
+++ AfterSchool1/andtest4/src/main/java/com/example/andtest4/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.andtest4
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.andtest4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    val helper = MyDBHelper(this)
+    val datas = mutableListOf<Todo>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var adapter = MyAdapter(datas)
+        datas.addAll(helper.allTodo())
+        binding.mainRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.mainRecyclerView.adapter = adapter
+
+        binding.fabAdd.setOnClickListener {
+            val intent = Intent(this, AddActivity::class.java)
+            startActivity(intent)
+        }
+
+        adapter.itemClick = object : MyAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+                val intent = Intent(applicationContext, AddActivity::class.java)
+                val todo = datas.get(position)
+                intent.putExtra("todo", todo)
+                startActivity(intent)
+            }
         }
     }
 }
